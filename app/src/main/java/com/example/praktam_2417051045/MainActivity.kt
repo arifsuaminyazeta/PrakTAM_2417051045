41a1612179e132d0de5f808d7f4d53075e96c7e8
arifsuaminyazeta <arifamu2236@gmail.com>
2026-03-19 20:13:03 +0700

    Tugas LKP 4

--- app/src/main/java/com/example/praktam_2417051045/MainActivity.kt
+++ app/src/main/java/com/example/praktam_2417051045/MainActivity.kt
@@ -7,19 +7,23 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.outlined.FavoriteBorder
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import com.example.praktam_2417051045.Model.Decision
 import com.example.praktam_2417051045.Model.DecisionSource
+import androidx.compose.foundation.background
+import androidx.compose.foundation.shape.CircleShape
 
 class MainActivity : ComponentActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -55,15 +59,45 @@ fun DecisionListScreen() {
 @Composable
 fun DecisionDetailScreen(decision: Decision) {
 
+    var isFavorite by remember { mutableStateOf(false) }
+
     Column {
 
-        Image(
-            painter = painterResource(id = decision.imageRes),
-            contentDescription = decision.title,
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(200.dp)
-        )
+        Box {
+
+            Image(
+                painter = painterResource(id = decision.imageRes),
+                contentDescription = decision.title,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(200.dp)
+            )
+
+            IconButton(
+                onClick = {
+                    isFavorite = !isFavorite
+                },
+                modifier = Modifier
+                    .align(Alignment.TopEnd)
+                    .padding(8.dp)
+            ) {
+                Box(
+                    modifier = Modifier
+                        .size(40.dp)
+                        .background(Color.Black.copy(alpha = 0.5f), shape = CircleShape),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Icon(
+                        imageVector = if (isFavorite)
+                            Icons.Filled.Favorite
+                        else
+                            Icons.Outlined.FavoriteBorder,
+                        contentDescription = "Favorite",
+                        tint = if (isFavorite) Color.Red else Color.White
+                    )
+                }
+            }
+        }
 
         Spacer(modifier = Modifier.height(8.dp))
 
